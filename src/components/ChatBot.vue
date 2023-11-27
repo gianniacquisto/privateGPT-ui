efb5c1ad327e567fa310a8c0571be91644471224
Gianni Acquisto <gianni.acquisto@gmail.com>
2023-11-27 23:02:29 +0100

    default random bot response

--- src/components/ChatBot.vue
+++ src/components/ChatBot.vue
@@ -21,27 +21,51 @@ export default {
     },
     methods: {
         sendMessage() {
-            const newMessageObj = {
+            const userMessage = this.newMessage;
+            const userMessageObj = {
                 id: this.chatMessages.length + 1,
                 sender: "User",
-                text: this.newMessage,
+                text: userMessage,
             };
 
             // Assuming you have a function to make the API call and handle the response
             // Replace this with your actual API call
-            this.callLLMAPI(newMessageObj.text);
+            this.callLLMAPI(userMessage);
 
-            this.chatMessages.push(newMessageObj);
+            this.chatMessages.push(userMessageObj);
             this.newMessage = ""; // Clear the input after sending
+
+            // Simulate a random response from the bot
+            const botResponse = this.generateRandomResponse();
+            const botResponseObj = {
+                id: this.chatMessages.length + 1,
+                sender: "Bot",
+                text: botResponse,
+            };
+
+            this.chatMessages.push(botResponseObj);
         },
         callLLMAPI(message) {
             // Make API call to your LLM with the message
             // Update the chatMessages with the response from the LLM
         },
+        generateRandomResponse() {
+            const responses = [
+                "I'm not sure what you mean.",
+                "That's interesting!",
+                "Tell me more about that.",
+                "I'm still learning!",
+                "Could you rephrase that?",
+                "I don't have the answer to that question.",
+            ];
+            const randomIndex = Math.floor(Math.random() * responses.length);
+            return responses[randomIndex];
+        },
     },
 };
 </script>
   
+ 
 <style scoped>
 .chat-bot {
     width: 70%;
@@ -89,5 +113,6 @@ export default {
     padding: 8px;
     border-radius: 5px;
     cursor: pointer;
-}</style>
+}
+</style>
   
