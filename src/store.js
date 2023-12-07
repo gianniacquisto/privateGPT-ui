e14b5c54b0abb462fca132cdf3d915d92dacc534
Gianni Acquisto <gianni.acquisto@gmail.com>
2023-12-07 14:20:53 +0100

    Saved the store to local storage

--- src/store.js
+++ src/store.js
@@ -1,7 +1,18 @@
-import { reactive } from 'vue'
+import { reactive, watch } from 'vue'
 
 export const store = reactive({
     chats: [],
     activeChatId: "",
     newChatSwitch: false
 })
+
+// Watch for changes in the store and update localStorage
+watch(() => store, () => {
+    localStorage.setItem('myAppStore', JSON.stringify(store))
+}, { deep: true })
+
+// Load the initial state from localStorage (if available)
+const storedState = localStorage.getItem('myAppStore')
+if (storedState) {
+    Object.assign(store, JSON.parse(storedState))
+}
